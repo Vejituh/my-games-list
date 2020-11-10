8c64b8381b8ea085d94cc24c69f64b3a60305aa8
Rafael Gomes <rafabear@bearmachine-share.broadband>
2020-11-10 17:00:44 +0000

    add e6+ and fix for ps file

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let displayDiv = document.getElementById("page-wrapper");
 let inputSearch = document.getElementById("search-game");
 let gameList = document.getElementById("game-list");
-let sortedArr = [];
+let sortedGamesList = [];
 let gamesArray;
 let sort = 0;
 let pastSort = 1;
@@ -10,44 +10,44 @@ let pastSort = 1;
 const letsFetch = () => {
     fetch('ps.txt').then(x => x.text()).then(games => {
         gamesArray = games.split('\n')
-        gamesArray.forEach((element) => {
-            sortedArr.push(element.split(","))
-        })
-       while (pastSort > 0) {
+        for (gameProps of gamesArray) {
+            sortedGamesList.push(gameProps.split(","))
+        }
+       while (pastSort >= 1) {
            sortArr();
        }
-       sortedArr.pop();
     })
 }
 
 const displayResults = () => {
         gameList.innerHTML = "";
-        sortedArr.forEach((element) => {
-            if (element[0].toLowerCase().includes(inputSearch.value.toLowerCase())) { 
+        for(const game of sortedGamesList){
+            let [gameTitle, yearOfRelease, platform, purchased] = game;
+            if (gameTitle.toLowerCase().includes(inputSearch.value.toLowerCase())) { 
                 let gameDiv = document.createElement("div");
-                 if (element[3]) {
+                 if (purchased) {
                     gameDiv.classList.add("bought");
                  }
                 gameDiv.classList.add("gameContainer");
-                let gameName = document.createElement("p");
-                let gameYear = document.createElement("p");
-                let gameConsole = document.createElement("p");
-                gameName.classList.add("game-name");
-                gameYear.classList.add("game-year");
-                gameConsole.classList.add("game-console");
-                gameName.textContent = element[0];
-                gameYear.textContent = element[1];
-                gameConsole.textContent = element[2];
-                gameDiv.appendChild(gameName);
-                gameDiv.appendChild(gameYear);
-                gameDiv.appendChild(gameConsole);
+                let displayGameName = document.createElement("p");
+                let displayGameYear = document.createElement("p");
+                let displayGameConsole = document.createElement("p");
+                displayGameName.classList.add("game-name");
+                displayGameYear.classList.add("game-year");
+                displayGameConsole.classList.add("game-console");
+                displayGameName.textContent = gameTitle;
+                displayGameYear.textContent = yearOfRelease;
+                displayGameConsole.textContent = platform;
+                gameDiv.appendChild(displayGameName);
+                gameDiv.appendChild(displayGameYear);
+                gameDiv.appendChild(displayGameConsole);
                 gameList.appendChild(gameDiv);
             }
-        })
+        }
 }
 
 const sortArr = () => {
-    sortedArr.sort(function(a,b){
+    sortedGamesList.sort(function(a,b){
         if(a[0] < b[0] && a[1] == b[1]) {
             sort ++;
             if(a[3] === "bought" && !b[3]) {
@@ -71,11 +71,5 @@ const sortArr = () => {
     pastSort = sort;
     sort = 0;
 }
-
-window.addEventListener("keydown", (e) => {
-    if(e.code === "Enter") {
-            displayResults();
-        }
-    });
 
     inputSearch.addEventListener('search', displayResults);
